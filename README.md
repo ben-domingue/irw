9d5b7f2e854e27abad33ef712e7584e3804ff330
ben-domingue <ben.domingue@gmail.com>
2024-02-28 11:42:55 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # irw
+
+data queue [here]{https://docs.google.com/spreadsheets/d/13EzVbybU6pIrTq6xiivLvcN9h5OMi3wGM9W-xASpMVI/edit#gid=1076583183}
+
---
+++ data/gilbert_hte/postprocessing.R
@@ -0,0 +1,40 @@
+##full formatting done in: gilbert_share/06 IL-HTE Econ/analysis/
+
+##goto 06 IL-HTE Econ/analysis/data/clean
+
+load("il_hte_data.Rdata")
+vars<-ls()
+L<-list()
+for (nm in vars) L[[nm]]<-get(nm)
+f<-function(x) {
+    id<-x$s_id
+    pretest<-x$std_baseline
+    resp<-x$score
+    treat<-x$treat
+    item<-x$item
+    data.frame(id=id,item=item,resp=resp,treatment=treat,pretest=pretest)
+}
+L<-lapply(L,f)
+
+lapply(L,function(x) table(x$resp))
+
+for (i in 1:length(L)) {
+    fn<-paste("/home/bdomingu/Dropbox/projects/irw/data/queue/",names(L)[i],".Rdata",sep='')
+    df<-L[[i]]
+    save(df,file=fn)
+}
+
+
+write.table(names(L),quote=FALSE,row.names=FALSE) #so that you can map data to paper
+##see here: https://psycnet.apa.org/doi/10.1037/dev0001710
+
+## kim2021_a, https://link.springer.com/article/10.1007/s10648-021-09609-6
+## kim2021_b, 
+## kim2021_c, 
+## kim2024_a, https://psycnet.apa.org/doi/10.1037/dev0001710
+## kim2024_b,https://psycnet.apa.org/doi/10.1037/dev0001710
+
+##already in irw. i would update to this version (ie delete old version; will also need to manage file names etc.
+## gilbert2023,https://journals.sagepub.com/doi/10.3102/10769986231171710
+## woods2021, https://doi.org/10.1371/journal.pone.0250545 ##already in irw?
+## kim2023, https://doi.org/10.1037/edu0000751
