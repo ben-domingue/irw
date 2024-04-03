48cb55baf8b78368b17525e4ebf96d7e843fcf3d
Hang Yu <161940036+anniehang@users.noreply.github.com>
2024-04-03 20:41:54 +0800

    Create vertifyage_study3c.R

---
+++ data/vertifyage_study3c.R
@@ -0,0 +1,25 @@
+#paper https://link.springer.com/article/10.3758/s13428-022-01944-y#Sec34
+#study_3c
+library(dplyr)
+library(tidyr)
+library(haven)
+
+data <- read_sav("Study 3c_IM_Data.sav")
+data <- data %>% 
+  mutate(across(everything(), ~`attr<-`(.x, "label", NULL)))
+
+data <- data[!is.na(data$Q4.4), ]
+
+data <- data %>%
+  select(ResponseId, Q33correct:Q59correct,Q4correct:Q47correct, Q4.4) %>%
+  rename(id = ResponseId, age = Q4.4)
+names(data) <- gsub("correct", "", names(data))
+
+data_1 <- data %>% 
+  pivot_longer(
+    cols = -c(id, age), 
+    names_to = "item", 
+    values_to = "resp"
+  )
+
+saveRDS(data_1, "vertifyage_study3c.RData")         
