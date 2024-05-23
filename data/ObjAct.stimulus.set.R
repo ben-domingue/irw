2ea9c28c73777dec29be46a0c35d28dc00d73d48
Ben Domingue <ben.domingue@gmail.com>
2024-05-23 13:15:32 -0700

    Update ObjAct.stimulus.set.R
    updating to stacked version of data

--- data/ObjAct.stimulus.set.R
+++ data/ObjAct.stimulus.set.R
@@ -1,4 +1,3 @@
-
 library(readxl)
 y <- read_excel("QPart1Data_objects.xlsx")
 x=y[, 19:190 ]
@@ -8,6 +7,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -20,7 +20,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df1=df
-save(df,file='Part1.Rdata')
 
 
 y <- read_excel("QPart2Data_objects.xlsx")
@@ -31,6 +30,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -43,7 +43,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df2=df
-save(df,file='Part2.Rdata')
 
 
 y <- read_excel("QPart3Data_objects.xlsx")
@@ -54,6 +53,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -66,7 +66,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df3=df
-save(df,file='Part3.Rdata')
 
 
 y <- read_excel("QPart4Data_objects.xlsx")
@@ -77,6 +76,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -89,7 +89,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df4=df
-save(df,file='Part4.Rdata')
 
 
 y <- read_excel("QPart5Data_objects.xlsx")
@@ -100,6 +99,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -112,7 +112,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df5=df
-save(df,file='Part5.Rdata')
 
 
 y <- read_excel("QPart6Data_objects.xlsx")
@@ -123,6 +122,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -135,8 +135,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df6=df
-save(df,file='Part6.Rdata')
-
 
 y <- read_excel("QPart7Data_objects.xlsx")
 x=y[, 19:190 ]
@@ -146,6 +144,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -158,8 +157,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df7=df
-save(df,file='Part7.Rdata')
-
 
 y <- read_excel("QPart8Data_objects.xlsx")
 x=y[, 19:190 ]
@@ -169,6 +166,7 @@ x=x[,-grep("Text", colnames(x)) ]
 x=x[, -grep("quality", colnames(x))]
 n=dim(x)[2]/8
 rater.id= y$IPAddress[-1]
+L=list()
 for (i in 1:n) L[[i]]<-x[,((i-1)*8+1):(i*8)]
 df=data.frame()
 for (i in 1:n){
@@ -181,5 +179,6 @@ for (i in 1:n){
   df=rbind(df, d)
 }
 df8=df
-save(df,file='Part8.Rdata')
 
+d=rbind(df1, df2, df3, df4, df5, df6, df7, df8)
+save(d, file='Object.stimulus.set.Rdata')
