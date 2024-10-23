8d72fd55ff7ed06552a13edcd39e8b2f71142028
saviranadela <32274458+saviranadela@users.noreply.github.com>
2024-10-22 17:12:19 -0700

    COACH_Chen_2022.R

--- data/COACH_Chen_2022.R
+++ data/COACH_Chen_2022.R
@@ -5,11 +5,6 @@ library(stringr)
 
 df <- read.xlsx("Copy of COACH_raw_data_22_5_16.xlsx")
 
-# unique_rows <- df %>%
-#   distinct(Cohort, Group, Number, Village)
-# 
-# print(nrow(unique_rows) == nrow(df)) # check if the number of rows in unique_rows is the same as df
-
 df$id <- seq(1, nrow(df))
 
 df <- df %>%
@@ -30,36 +25,56 @@ df <- df %>%
                names_to = "item", 
                values_to = "resp") %>%
   filter(!is.na(resp)) %>%
+  mutate_all(tolower) %>%
   mutate(wave = case_when(
-    str_detect(item, 'Baseline') ~ 1,
-    str_detect(item, 'RA_Twelfth_Month_Treatment_Stigma') ~ 2,
-    str_detect(item, 'RA_First_Month_HDRS') ~ 2,
-    str_detect(item, 'RA_Third_Month_HDRS') ~ 3,
-    str_detect(item, 'RA_Sixth_Month_HDRS') ~ 4,
-    str_detect(item, 'RA_Ninth_Month_HDRS') ~ 5,
-    str_detect(item, 'RA_Twelfth_Month_HDRS') ~ 6,
-    str_detect(item, 'RA_Sixth_Month_WHOQOL_BREF') ~ 2,
-    str_detect(item, 'RA_Twelfth_Month_WHOQOL_BREF') ~ 3,
-    str_detect(item, 'RA_Sixth_Month_ADL') ~ 2,
-    str_detect(item, 'RA_Twelfth_Month_ADL') ~ 3,
-    str_detect(item, 'RA_Sixth_Month_IADL') ~ 2,
-    str_detect(item, 'RA_Twelfth_Month_IADL') ~ 3,
-    str_detect(item, 'RA_Sixth_Month_MOS_SSS_C') ~ 2,
-    str_detect(item, 'RA_Twelfth_Month_MOS_SSS_C') ~ 3,
-    str_detect(item, 'RA_Twelfth_Month_Social_Network') ~ 2,
-    str_detect(item, 'PCP_First_Month_PHQ9') ~ 2,
-    str_detect(item, 'PCP_Third_Month_PHQ9') ~ 3,
-    str_detect(item, 'PCP_Sixth_Month_PHQ9') ~ 4,
-    str_detect(item, 'PCP_Ninth_Month_PHQ9') ~ 5,
-    str_detect(item, 'PCP_Twelfth_Month_PHQ9') ~ 6,
+    str_detect(item, 'baseline') ~ 1,
+    str_detect(item, 'ra_twelfth_month_treatment_stigma') ~ 2,
+    str_detect(item, 'ra_first_month_hdrs') ~ 2,
+    str_detect(item, 'ra_third_month_hdrs') ~ 3,
+    str_detect(item, 'ra_sixth_month_hdrs') ~ 4,
+    str_detect(item, 'ra_ninth_month_hdrs') ~ 5,
+    str_detect(item, 'ra_twelfth_month_hdrs') ~ 6,
+    str_detect(item, 'ra_sixth_month_whoqol_bref') ~ 2,
+    str_detect(item, 'ra_twelfth_month_whoqol_bref') ~ 3,
+    str_detect(item, 'ra_sixth_month_adl') ~ 2,
+    str_detect(item, 'ra_twelfth_month_adl') ~ 3,
+    str_detect(item, 'ra_sixth_month_iadl') ~ 2,
+    str_detect(item, 'ra_twelfth_month_iadl') ~ 3,
+    str_detect(item, 'ra_sixth_month_mos_sss_c') ~ 2,
+    str_detect(item, 'ra_twelfth_month_mos_sss_c') ~ 3,
+    str_detect(item, 'ra_twelfth_month_social_network') ~ 2,
+    str_detect(item, 'pcp_first_month_phq9') ~ 2,
+    str_detect(item, 'pcp_third_month_phq9') ~ 3,
+    str_detect(item, 'pcp_sixth_month_phq9') ~ 4,
+    str_detect(item, 'pcp_ninth_month_phq9') ~ 5,
+    str_detect(item, 'pcp_twelfth_month_phq9') ~ 6,
     TRUE ~ NA_real_
   )) %>%
   mutate(item = case_when(
-    str_detect(item, 'Baseline') ~ str_replace(item, "(.?)Baseline", ""),
-    str_detect(item, 'Month') ~ str_replace(item, "^([^_]+)_[^_]+_Month_(.*)", "\\1_\\2"),
+    str_detect(item, 'baseline') ~ str_replace(item, "(.?)baseline", ""),
+    str_detect(item, 'month') ~ str_replace(item, "^([^_]+)_[^_]+_month_(.*)", "\\1_\\2"),
     TRUE ~ item
   )) %>%
   select(id, item, wave, resp) %>%
   filter(!resp %in% c(6, 7, 11, 12, 21, 22, 32, 33, 55))
 
-write.csv(df, "COACH_Chen_2022.csv", row.names=FALSE)
+df_treatment <- df %>% filter(grepl("treatment", item))
+df_hdrs <- df %>% filter(grepl("hdrs", item))
+df_whoqol <- df %>% filter(grepl("whoqol", item))
+df_csq <- df %>% filter(grepl("customer", item)) %>%
+          select (id, resp, item)
+df_adl <- df %>% filter(grepl("_adl", item))
+df_iadl <- df %>% filter(grepl("iadl", item))
+df_mos <- df %>% filter(grepl("mos", item))
+df_sns <- df %>% filter(grepl("social", item))
+df_phq <- df %>% filter(grepl("phq", item))
+
+write.csv(df_treatment, "COACH_Chen_2022_treatmentStigma.csv", row.names=FALSE)
+write.csv(df_hdrs, "COACH_Chen_2022_HDRS.csv", row.names=FALSE)
+write.csv(df_whoqol, "COACH_Chen_2022_WHOQOL_BREF.csv", row.names=FALSE)
+write.csv(df_csq, "COACH_Chen_2022_CSQ.csv", row.names=FALSE)
+write.csv(df_adl, "COACH_Chen_2022_ADL.csv", row.names=FALSE)
+write.csv(df_iadl, "COACH_Chen_2022_IADL.csv", row.names=FALSE)
+write.csv(df_mos, "COACH_Chen_2022_MOS_SSS_C.csv", row.names=FALSE)
+write.csv(df_sns, "COACH_Chen_2022_SNS.csv", row.names=FALSE)
+write.csv(df_phq, "COACH_Chen_2022_PHQ9.csv", row.names=FALSE)
