0b7fa17d84dfb4531ec6cc716bf4a44b477dc81a
KingArthur0205 <gpan20020205@gmail.com>
2024-10-28 14:35:42 +0000

    Fix for Pivot

--- data/QCDQES_Oliveira_2022.R
+++ data/QCDQES_Oliveira_2022.R
@@ -13,6 +13,8 @@ df[] <- lapply(df, function(col) { # Remove column labels for each column
 df <- df |>
   mutate(id=row_number()) |>
   select(id, starts_with("OCDQ"), -ends_with("R"))
+df <-pivot_longer(df, names_to="item", values_to="resp", cols=-id)
 
 save(df, file="QCDQES_Oliveira_2022.Rdata")
 write.csv(df, "QCDQES_Oliveira_2022.csv", row.names=FALSE)
+
