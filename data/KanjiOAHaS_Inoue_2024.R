63067e388b7e142c2aec2217543cf146ca28db74
saviranadela <32274458+saviranadela@users.noreply.github.com>
2024-11-20 14:48:32 -0800

    KanjiOAHaS_Inoue_2024

--- data/KanjiOAHaS_Inoue_2024.R
+++ data/KanjiOAHaS_Inoue_2024.R
@@ -4,7 +4,8 @@ df <- read.csv("Raw_Data_20240607.csv")
 
 df <- df %>%
   select(student_id, item, score, total_time) %>%
-  rename(id = student_id, resp = score, rt = total_time)
+  rename(id = student_id, resp = score, rt = total_time) %>%
+  filter(!is.na(resp))
 
 
 write.csv(df, "KanjiOAHaS_Inoue_2024.csv", row.names=FALSE)
