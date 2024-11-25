71075d5c2d3ccbb379775b3d32b46c20f2697422
Ben Domingue <ben.domingue@gmail.com>
2024-11-25 12:43:10 -0800

    .

--- preprint_src/00_getdata.R
+++ preprint_src/00_getdata.R
@@ -3,11 +3,11 @@
 
 ## Get a sample dataset from the Redivis platform
 ## Access the user "datapages" on Redivis
-dataset <- redivis::user("datapages")$dataset("item_response_warehouse", version = "v4.0")
+dataset <- redivis::user("datapages")$dataset("item_response_warehouse")
 
 # Extract the specific table named "kim2023" from the dataset
 ## Then convert this table into a data frame for easier data manipulation
-df <- dataset$table("kim2023")$to_data_frame()
+df <- dataset$table("gilbert_meta_2")$to_data_frame()
 
 ## Now 'df' contains the data from the "kim2023" table as a data frame
 ## You can proceed to analyze 'df' or perform data operations.
