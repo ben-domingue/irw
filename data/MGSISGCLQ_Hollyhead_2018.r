a1663d68e983ad48c2c537c7d79321bd4e22b93b
KingArthur0205 <gpan20020205@gmail.com>
2024-11-28 01:25:17 +0000

    Added for Covariates

--- data/MGSISGCLQ_Hollyhead_2018.r
+++ data/MGSISGCLQ_Hollyhead_2018.r
@@ -13,14 +13,16 @@ df <- df |>
 df <- df[!apply(df[, -which(names(df) == "id")], 1, function(row) all(is.na(row))), ]
 
 mgsis_df <- df |>
-  select(id, starts_with("I"))
+  select(id, starts_with("I"), `Do.you.have.a.medical.condition.which.affects.your.lower.body.or.could.impact.how.you.feel.about.your.genitals?`) |>
+  rename(cov_medical_condition=`Do.you.have.a.medical.condition.which.affects.your.lower.body.or.could.impact.how.you.feel.about.your.genitals?`)
 GCLQ_df <- df |>
-  select(-starts_with("I"), id, -`Do.you.have.a.medical.condition.which.affects.your.lower.body.or.could.impact.how.you.feel.about.your.genitals?`)
+  select(-starts_with("I"), id) |>
+  rename(cov_medical_condition=`Do.you.have.a.medical.condition.which.affects.your.lower.body.or.could.impact.how.you.feel.about.your.genitals?`)
 
 GCLQ_df[] <- lapply(GCLQ_df, function(x) ifelse(x == "Yes", 1, ifelse(x == "No", 0, x)))
 GCLQ_df <- GCLQ_df %>%
   mutate(across(everything(), ~ as.numeric(as.character(.))))
-GCLQ_df <- pivot_longer(GCLQ_df, cols=-id, names_to="item", values_to="resp")
+GCLQ_df <- pivot_longer(GCLQ_df, cols=-c(id, cov_medical_condition), names_to="item", values_to="resp")
 
 likert_map <- c(
   "Strongly Disagree" = 1,
@@ -29,11 +31,11 @@ likert_map <- c(
   "Strongly Agree" = 4
 )
 
-mgsis_df <- pivot_longer(mgsis_df, cols=-id, names_to="item", values_to="resp")
+mgsis_df <- pivot_longer(mgsis_df, cols=-c(id, cov_medical_condition), names_to="item", values_to="resp")
 mgsis_df$resp <- likert_map[mgsis_df$resp]
-print(table(GCLQ_df$item))
 
 final_df <- rbind(mgsis_df, GCLQ_df)
+final_df$cov_medical_condition <- as.numeric(lapply(final_df$cov_medical_condition, function(x) ifelse(x == "Yes", 1, ifelse(x == "No", 0, x))))
 
 save(final_df, file="MGSISGCLQ_Hollyhead_2018.Rdata")
 write.csv(final_df, "MGSISGCLQ_Hollyhead_2018.csv", row.names=FALSE)
