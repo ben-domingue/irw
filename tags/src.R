e75c0028c3d095a6876f4291a3e0b6a1890cab4b
Ben Domingue <ben.domingue@gmail.com>
2025-02-06 06:27:13 -0800

    tags

--- tags/src.R
+++ tags/src.R
@@ -1,5 +1,15 @@
 tabs<-irwpkg::irw_list_datasets()
+tabs<-tolower(tabs$name)
 
 library(gsheet)
 work<-gsheet2tbl('https://docs.google.com/spreadsheets/d/1V3ef0sa7HKtJJd2cgqRAkEdfbpGWDD1JIyQa6HwVK7g/edit?gid=126134123#gid=126134123')
-tabs.work<-work$Table
+tabs.done<-work$Table
+
+all(tabs.done %in% tabs) ##confirming all is well
+
+index<-tabs %in% tabs.done ##to remove
+tabs.todo<-tabs[!index]
+any(tabs.todo %in% tabs.done) ##confirming all is well
+
+z<-sample(tabs.todo,30)
+write.csv(z,'',quote=FALSE,row.names=FALSE)
