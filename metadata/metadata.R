f35121bf996cee2306d2556e9fb0e65bdd84c553
Ben Domingue <ben.domingue@gmail.com>
2025-02-13 16:36:16 -0800

    adding to biblio

--- metadata/metadata.R
+++ metadata/metadata.R
@@ -8,8 +8,8 @@ dataset <- user$dataset("irw_meta:bdxt:latest")
 table <- dataset$table("metadata:h5gs")
 meta <- table$to_tibble()
 meta<-meta[,c("table", "n_responses", "n_categories", "n_participants", 
-"n_items", "responses_per_participant", "responses_per_item", 
-"density")]
+              "n_items", "responses_per_participant", "responses_per_item", 
+              "density")]
 dim(meta)
 old.tables<-meta$table
 length(old.tables)
@@ -34,69 +34,69 @@ old.tables[!torem]
 dim(meta)
 ii<-match(old.tables[!torem],meta$table)
 if (length(ii)>0) {
-    meta[ii,]
-    meta<-meta[-ii,]
+  meta[ii,]
+  meta<-meta[-ii,]
 }
 dim(meta)
 
 f<-function(tab) {
-    print(tab)
-    variables <- tab$list_variables() 
-    nms<-sapply(variables,function(x) x$get()$properties$name)
-    stats<-lapply(variables,function(x) x$properties$statistics) #stats<-lapply(variables,function(x) x$get()$properties$statistics)
-    names(stats)<-nms
-    n_responses<-stats$resp$count
-    if (is.null(n_responses)) {
-        df <- tab$to_tibble()
-        df<-df[!is.na(df$resp),]
-        n_responses<-length(df$resp)
-    }
-    n_categories<-stats$resp$numDistinct
-    n_participants<-stats$id$numDistinct
-    n_items<-stats$item$numDistinct
-    responses_per_participant = n_responses / n_participants
-    responses_per_item = n_responses / n_items
-    density = (sqrt(n_responses) / n_participants) * (sqrt(n_responses) / n_items)
-    ##throttle
-    i<-0
-    while (i<10000000) i<-i+1
-    ##
-    data.frame(
-            n_responses=n_responses,
-            n_categories=n_categories,
-            n_participants=n_participants,
-            n_items=n_items,
-            responses_per_participant=responses_per_participant,
-            responses_per_item=responses_per_item,
-            density=density
-    )
+  print(tab)
+  variables <- tab$list_variables() 
+  nms<-sapply(variables,function(x) x$get()$properties$name)
+  stats<-lapply(variables,function(x) x$properties$statistics) #stats<-lapply(variables,function(x) x$get()$properties$statistics)
+  names(stats)<-nms
+  n_responses<-stats$resp$count
+  if (is.null(n_responses)) {
+    df <- tab$to_tibble()
+    df<-df[!is.na(df$resp),]
+    n_responses<-length(df$resp)
+  }
+  n_categories<-stats$resp$numDistinct
+  n_participants<-stats$id$numDistinct
+  n_items<-stats$item$numDistinct
+  responses_per_participant = n_responses / n_participants
+  responses_per_item = n_responses / n_items
+  density = (sqrt(n_responses) / n_participants) * (sqrt(n_responses) / n_items)
+  ##throttle
+  i<-0
+  while (i<10000000) i<-i+1
+  ##
+  data.frame(
+    n_responses=n_responses,
+    n_categories=n_categories,
+    n_participants=n_participants,
+    n_items=n_items,
+    responses_per_participant=responses_per_participant,
+    responses_per_item=responses_per_item,
+    density=density
+  )
 }
 out<-list()
 
 nms<-new.tables[!toadd]
 ii<-match(nms,new.tables)
 if (length(ii)>0) {
-    for (i in ii) {
-        print(which(i==ii))
-        out[[as.character(i)]]<-f(tables[[i]])
-    }
-    summaries<-data.frame(do.call("rbind",out))
-    summaries$table<-nms[1:nrow(summaries)]
-    library(tidyr)
-    summaries_new<-as_tibble(summaries)
-    length(ii)
-    dim(summaries_new)
-    head(meta)
-    head(summaries_new)
-    nms.cols<-names(meta)
-    for (nm in nms.cols) {
-        test<-nm %in% names(summaries_new)
-        if (!test) summaries_new[[nm]]<-NA
-    }
-    summaries_new<-summaries_new[,nms.cols]
-    summaries<-as_tibble(rbind(meta,summaries_new))
+  for (i in ii) {
+    print(which(i==ii))
+    out[[as.character(i)]]<-f(tables[[i]])
+  }
+  summaries<-data.frame(do.call("rbind",out))
+  summaries$table<-nms[1:nrow(summaries)]
+  library(tidyr)
+  summaries_new<-as_tibble(summaries)
+  length(ii)
+  dim(summaries_new)
+  head(meta)
+  head(summaries_new)
+  nms.cols<-names(meta)
+  for (nm in nms.cols) {
+    test<-nm %in% names(summaries_new)
+    if (!test) summaries_new[[nm]]<-NA
+  }
+  summaries_new<-summaries_new[,nms.cols]
+  summaries<-as_tibble(rbind(meta,summaries_new))
 } else {
-    summaries<-meta
+  summaries<-meta
 }
 
 str(summaries)
@@ -138,17 +138,16 @@ fetch_bibtex_from_doi <- function(filename, doi) {
   if (is.na(doi) || doi == "") {
     return(NA_character_)  # Return NA if DOI is missing
   }
-  
   url <- paste0("https://doi.org/", doi)
   response <- tryCatch({
-      GET(url, add_headers(Accept = "application/x-bibtex"))
-    },
-    error = function(e) {
-      warning(glue("Error fetching dataset: {filename} - {e$message}"))
-      return(NULL)
-    }
+    GET(url, add_headers(Accept = "application/x-bibtex"))
+  },
+  error = function(e) {
+    warning(glue("Error fetching dataset: {filename} - {e$message}"))
+    return(NULL)
+  }
   )
-  
+  ##
   if (!is.null(response) && status_code(response) == 200) {
     return(content(response, as = "text", encoding = "UTF-8"))
   } else {
@@ -170,7 +169,7 @@ head(biblio)
 # Find rows in dictionary whose Filename is not in biblio
 new_data_rows <- irw_dict[!(irw_dict$table %in% biblio$table), ]
 new_data_rows <- new_data_rows |>
-  select(table, Reference, `DOI (for paper)`) |>
+  select(table, Reference, `DOI (for paper)`, Description, `URL (for data)`) |>
   rename(DOI=`DOI (for paper)`)
 new_data_rows <- new_data_rows %>%
   mutate(BibTex = map2_chr(table, DOI, fetch_bibtex_from_doi))
