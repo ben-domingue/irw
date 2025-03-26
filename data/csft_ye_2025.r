8543a3cc003abc8636163c073175e3ef83035beb
KingArthur0205 <gpan20020205@gmail.com>
2025-03-26 17:50:57 +0000

    Updated

--- data/csft_ye_2025.r
+++ data/csft_ye_2025.r
@@ -7,9 +7,9 @@ library(tidyr)
 df <- read.csv("13428_2024_2559_MOESM1_ESM.csv")
 df <- pivot_longer(df, cols=-ID, names_to="item", values_to="resp")
 df <- df |>
-  rename(id=ID, cov_weight=resp)
-df$resp <- ifelse(df$cov_weight > 0, 1, 0)
-df$cov_weight <- ifelse(df$item %in% c("X1", "X2", "X11", "X12"), 0.5,
+  rename(id=ID, itemcov_weight=resp)
+df$resp <- ifelse(df$itemcov_weight > 0, 1, 0)
+df$itemcov_weight <- ifelse(df$item %in% c("X1", "X2", "X11", "X12"), 0.5,
                         ifelse(df$item %in% c("X3", "X4", "X13", "X14"), 1,
                                1.5))
 
