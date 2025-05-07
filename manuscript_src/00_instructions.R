66a3b22ae3e820fc6b0fb08b362e503bf55079d9
Ben Domingue <ben.domingue@gmail.com>
2025-05-07 15:18:58 -0700

    ...

--- manuscript_src/00_instructions.R
+++ manuscript_src/00_instructions.R
@@ -1,4 +1,5 @@
 ## Load the 'redivis' package (ensure it's installed)
 ## If it's not installed, you can do that with: devtools::install_github("redivis/redivis-r", ref="main")
 ## You will also need the 'irw' R package: devtools::install_github("itemresponsewarehouse/Rpkg")
+## More instructions on this package are available at: https://github.com/itemresponsewarehouse/Rpkg
 
