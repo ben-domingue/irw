8995b264739af47a073b408bc59f158d0e66e06c
Ben Domingue <ben.domingue@gmail.com>
2025-05-12 05:39:53 -0700

    code

--- manuscript_src/01_simpleIRT.R
+++ manuscript_src/01_simpleIRT.R
@@ -1,8 +1,8 @@
-df<-irw::irw_fetch("gilbert_meta_2")
+df<-irw::irw_fetch("gilbert_meta_2") ##fetch data from the IRW
 df$item <- paste("item_", df$item, sep = "")
-resp <- irwpkg::irw_long2resp(df)
+resp <- irwpkg::irw_long2resp(df) ##reformat long data to wide
 resp$id <- NULL
-psych::alpha(resp)
-paran::paran(resp)
-psych::fa(cov(resp), nfactors = 3)
-mirt::mirt(resp, 1, "Rasch")
+psych::alpha(resp) ##compute cronbach's alpha
+paran::paran(resp) ##parallel analysis to estimate dimensionality
+psych::fa(cov(resp), nfactors = 3) ##exploratory factor analysis
+mirt::mirt(resp, 1, "Rasch") ##fitting the Rasch model
