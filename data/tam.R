a4146338f888e249ecd4f9bb073b8c1df81e3d64
Ben Domingue <ben.domingue@gmail.com>
2025-06-30 15:31:44 -0700

    Update tam.R

--- data/tam.R
+++ data/tam.R
@@ -39,3 +39,36 @@ L<-list()
 for (i in ii) L[[i]]<-data.frame(id=id,item=names(x)[i],country=x$IDCNTRY,booklet=x$IDBOOK,resp=x[,i])
 df<-data.frame(do.call("rbind",L))
 save(df,file="timss_tam.Rdata")
+##
+# Data fix
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+library(readr)
+library(dplyr)
+
+timss_tam <- read_csv("timss_tam.csv")
+
+# Set missing values
+timss_tam$resp[timss_tam$resp %in% c(6, 9, 96, 99)] <- NA
+
+# Recode CR items
+timss_tam$resp[timss_tam$resp %in% c(20, 21)] <- 2    # fully correct
+timss_tam$resp[timss_tam$resp %in% c(10, 11)] <- 1    # partially correct
+timss_tam$resp[timss_tam$resp %in% c(70, 79)] <- 0    # incorrect
+
+# Recode MC items
+correct_keys <- c(
+  "M032166" = 2,
+  "M032721" = 2,
+  "M032626" = 4,
+  "M032595" = 3,
+  "M032673" = 3
+)
+
+timss_tam <- timss_tam %>%
+  mutate(resp = if_else(
+    item %in% names(correct_keys) & !is.na(resp),
+    as.numeric(resp == correct_keys[item]),
+    resp
+  ))
+
+write_csv(timss_tam, "timss_tam.csv")
