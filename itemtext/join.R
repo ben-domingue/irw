7ab93bed24e9e70a63b4f18cd0d95f6cdb58a361
Ben Domingue <ben.domingue@gmail.com>
2025-07-10 07:03:26 -0700

    item text

---
+++ itemtext/join.R
@@ -0,0 +1,21 @@
+table<-'coach_chen_2022_phq9'
+library(gsheet)
+tabs <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1jvwxYJ3gjSpEDtx4km-8czvDXu7iEIHhF5V5Y9VWNG0/edit?gid=0#gid=0')
+tabs$table<-tolower(tabs$table)
+f<-function(table,tabs) {
+    L<-list()
+    ii<-grep(table,tabs$table)
+    for (i in 1:4) L[[i]]<-gsheet2tbl(tabs[ii,i+2])
+    L
+}
+L<-f(table,tabs) ##note that i changed to lowercase
+
+##lapply(L,names)
+items<-L[[1]]
+for (i in 2:length(L)) items<-merge(items,L[[i]],all.x=TRUE)
+
+df<-irw::irw_fetch(table) ##see authentication and installation notes here. https://github.com/ben-domingue/irwpkg
+unique(df$item)
+unique(items$item)
+
+write.csv(items,file=paste(table,"__items.csv",sep=''))
