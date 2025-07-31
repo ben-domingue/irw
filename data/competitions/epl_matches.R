2121e69b8f876e724b32c22cbbd2fb95f1e3dbc8
Ben Domingue <ben.domingue@gmail.com>
2025-07-31 07:14:37 -0700

    comps

---
+++ data/competitions/epl_matches.R
@@ -0,0 +1,12 @@
+x<-read.csv("matches.csv")
+x<-x[x$venue=="Home",]
+
+delta<-x$gf-x$ga
+
+test<-delta>=0
+id_1<-ifelse(test,x$team,x$opponent)
+id_2<-ifelse(!test,x$team,x$opponent)
+df<-data.frame(id_1=id_1,id_2=id_2,resp=abs(delta))
+
+df$date<-as.numeric(strptime(x$date,format='%Y-%m-%d'))
+save(df,file="epl_matches_2021-2022.Rdata")
